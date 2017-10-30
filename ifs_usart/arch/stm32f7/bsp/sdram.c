41de704b12e37dddd601f25fbc2bbb5424539043
liquanqing <liquanqing138@gmail.com>
2017-10-30 08:25:29 +0800

    update sdram.c(not use)

--- ifs_usart/arch/stm32f7/bsp/sdram.c
+++ ifs_usart/arch/stm32f7/bsp/sdram.c
@@ -6,6 +6,7 @@
  ************************************************************************/
 #include "ifs.h"
 #include "gpio.h"
+#include "sdram.h"
 #include "stm32f7xx.h"
 
 
@@ -16,10 +17,6 @@
  */
 static void sdram_gpio_config(void)
 {
-    uint32_t sd_ctrl_reg = 0, sd_time_reg = 0;
-    uint16_t m_reg_val = 0;
-
-    RCC->AHB3ENR |= 1 << 0; // enable fmc clk
     gpio_init(IFS_GPIOD);
     gpio_init(IFS_GPIOE);
     gpio_init(IFS_GPIOF);
@@ -28,22 +25,77 @@ static void sdram_gpio_config(void)
     gpio_init(IFS_GPIOI);
     /*
      * FMC_A0~A12: GPIOF_0~5(A0~A5), GPIOF_12~15)(A6~A9), GPIOG_0~2(A10~A12)
-     * FMC_D0~D31: GPIOD_14~15(D0~D1), GPIOD_0~1(D2~D3), GPIOE_7~15(D4~D12)
+     * FMC_D0~D31: GPIOD_14~15(D0~D1), GPIOD_0~1(D2~D3), GPIOE_7~15(D4~D12) GPIOD_8~10(D13~D15)
      *             GPIOH_8~15((D16~D23), GPIOI_0~3(D24~D27), GPIOI_6~7(D28~D29)
                    GPIOI_9~10(D30~D31)
-     * FMC_BA1~0 : GPIOG_4~5(A0~A1)
-     * FMC_NBL0~3: GPIOE_0~1(NBL0~NBL1) GPIOI_4~5(NBL2~NBL3)
      * FMC_SDNWE : GPIOH_5
      * FMC_SDNRAS: GPIOF_11
      * FMC_SDNCAS: GPIOG_15
      * FMC_SDCLK : GPIOG_8
+     * FMC_BA1~0 : GPIOG_4~5(A0~A1)
+     * FMC_NBL0~3: GPIOE_0~1(NBL0~NBL1) GPIOI_4~5(NBL2~NBL3)
      * FMC_SDNE0 : GPIOH_3
      * FMC_SDCKE0: GPIOH_2
-     * FMC_SDNWE : GPIOH_5
      */
+    gpio_config(IFS_GPIOD, 
+                IFS_PIN_14 | IFS_PIN_15 | IFS_PIN_0 | IFS_PIN_1 | IFS_PIN_8 | IFS_PIN_9 | IFS_PIN_10, 
+                IFS_GPIO_ALTERNATE);
+    gpio_config(IFS_GPIOE,
+                IFS_PIN_7 | IFS_PIN_8 | IFS_PIN_9 | IFS_PIN_10 | IFS_PIN_11 | IFS_PIN_12 | IFS_PIN_13 | IFS_PIN_14 | IFS_PIN_15 | IFS_PIN_0 | IFS_PIN_1,
+                IFS_GPIO_ALTERNATE);
+    gpio_config(IFS_GPIOF,
+                IFS_PIN_0 | IFS_PIN_1 | IFS_PIN_2 | IFS_PIN_3 | IFS_PIN_4 | IFS_PIN_5 | IFS_PIN_11 | IFS_PIN12 | IFS_PIN13 | IFS_PIN_14 | IFS_PIN_15,
+                IFS_GPIO_ALTERNATE);
+    gpio_config(IFS_GPIOG,
+                IFS_PIN_0 | IFS_PIN_1 | IFS_PIN_2 | IFS_PIN_4 | IFS_PIN_5 | IFS_PIN_8 | IFS_PIN_15,
+                IFS_GPIO_ALTERNATE);
+    gpio_config(IFS_GPIOH,
+                IFS_PIN_2 | IFS_PIN_3 | IFS_PIN_5 | IFS_PIN_8 | IFS_PIN_9 | IFS_PIN_10 | IFS_PIN_11 | IFS_PIN_12 | IFS_PIN_13 | IFS_PIN_14 | IFS_PIN_15,
+                IFS_GPIO_ALTERNATE);
+    gpio_config(IFS_GPIOI,
+                IFS_PIN_0 | IFS_PIN_1 | IFS_PIN_2 | IFS_PIN_3 | IFS_PIN_4 | IFS_PIN_5 | IFS_PIN_6 | IFS_PIN_7 | IFS_PIN_9 | IFS_PIN_10,
+                IFS_GPIO_ALTERNATE);
 
+}
 
+/*
+ * !brief init sdram sequence
+ * !param none
+ * !retval none
+ */
+static void sdram_init_sequence(void)
+{
+    uint32_t tmp_r1 = 0, tmp_r2  = 0;
 
+    RCC->AHB3ENR |= 1 << 0; // enable fmc clk
 
+    tmpr1 = FMC_Bank5_6->SDCR[FMC_SDRAM_OFFSET];
+    /* Clear NC, NR, MWID, NB, CAS, WP, SDCLK, RBURST, and RPIPE bits */
+    tmpr1 &= ((uint32_t)~(FMC_SDCR1_NC  | FMC_SDCR1_NR | FMC_SDCR1_MWID | \
+                          FMC_SDCR1_NB  | FMC_SDCR1_CAS | FMC_SDCR1_WP   | \
+                          FMC_SDCR1_SDCLK | FMC_SDCR1_RBURST | FMC_SDCR1_RPIPE));
 
+    tmpr1 |= (uint32_t)(SDRAM_CTRL_PARAM);                                      
+    FMC_Bank5_6->SDCR[FMC_SDRAM_OFFSET] = tmpr1;
+
+    tmpr1 = FMC_Bank5_6->SDTR[FMC_SDRAM_OFFSET];
+    /* Clear TMRD, TXSR, TRAS, TRC, TWR, TRP and TRCD bits */
+    tmpr1 &= ((uint32_t)~(FMC_SDTR1_TMRD  | FMC_SDTR1_TXSR | FMC_SDTR1_TRAS | \
+                          FMC_SDTR1_TRC  | FMC_SDTR1_TWR | FMC_SDTR1_TRP | \
+                          FMC_SDTR1_TRCD));
+    
+    tmpr1 |= (uint32_t)(((LOAD_TO_ACTIVE_DELAY)-1)           |\
+                       (((EXIT_SELF_REFRESH_DELAY)-1) << 4) |\
+                       (((SELF_REFRESH_TIME)-1) << 8)      |\
+                       (((ROW_CYCLE_DELAY)-1) << 12)       |\
+                       (((WRITE_RECOVERY_TIME)-1) <<16)    |\
+                       (((RPDELAY)-1) << 20)             |\
+                       (((RCD_DELAY)-1) << 24));
+    Device->SDTR[FMC_SDRAM_BANK1] = tmpr1;
+}
+
+void sdram_init(void)
+{
+    sdram_gpio_config();
+    sdram_init_sequence();
 }
--- ifs_usart/arch/stm32f7/bsp/sdram.h
+++ ifs_usart/arch/stm32f7/bsp/sdram.h
@@ -7,4 +7,75 @@
 
 #ifndef _SDRAM_H
 #define _SDRAM_H
+
+/*
+ * chip 
+ */
+#define MT48LC4M32B2B5 0
+
+
+#define SDRAM_CHIP MT48LC4M32B2B5
+
+#define SDRAM_BANK_OFFSET                   0
+
+#define SDRAM_COLUMN_8BIT                   ((uint32_t)0x00000000U)
+#define SDRAM_COLUMN_9BIT                   ((uint32_t)0x00000001U)
+#define SDRAM_COLUMN_10BIT                  ((uint32_t)0x00000002U)
+#define SDRAM_COLUMN_11BIT                  ((uint32_t)0x00000003U)
+
+#define SDRAM_ROW_11BIT                     ((uint32_t)0x00000000U)
+#define SDRAM_ROW_12BIT                     ((uint32_t)0x00000004U)
+#define SDRAM_ROW_13BIT                     ((uint32_t)0x00000008U)
+
+#define SDRAM_CAS_LATENCY_1                 ((uint32_t)0x00000080U)
+#define SDRAM_CAS_LATENCY_2                 ((uint32_t)0x00000100U)
+#define SDRAM_CAS_LATENCY_3                 ((uint32_t)0x00000180)
+
+#define SDRAM_MEM_BUS_WIDTH_8               ((uint32_t)0x00000000U)
+#define SDRAM_MEM_BUS_WIDTH_16              ((uint32_t)0x00000010U)
+#define SDRAM_MEM_BUS_WIDTH_32              ((uint32_t)0x00000020U)
+
+#define SDRAM_INTERN_BANKS_NUM_2            ((uint32_t)0x00000000U)
+#define SDRAM_INTERN_BANKS_NUM_4            ((uint32_t)0x00000040U)
+
+#define SDRAM_WRITE_PROTECTION_DISABLE      ((uint32_t)0x00000000U)
+#define SDRAM_WRITE_PROTECTION_ENABLE       ((uint32_t)0x00000200U)
+
+#define SDRAM_CLOCK_DISABLE                 ((uint32_t)0x00000000U)
+#define SDRAM_CLOCK_PERIOD_2                ((uint32_t)0x00000800U)
+#define SDRAM_CLOCK_PERIOD_3                ((uint32_t)0x00000C00)
+
+#define SDRAM_RBURST_DISABLE                ((uint32_t)0x00000000U)
+#define SDRAM_RBURST_ENABLE                 ((uint32_t)0x00001000U)
+
+#define SDRAM_RPIPE_DELAY_0               ((uint32_t)0x00000000U)
+#define SDRAM_RPIPE_DELAY_1               ((uint32_t)0x00002000U)
+#define SDRAM_RPIPE_DELAY_2               ((uint32_t)0x00004000U)
+
+#if (SDRAM_CHIP == MT48LC4M32B2B5-6A)
+#define SDRAM_REFRESH_COUNT                 ((uint32_t)0x0603)
+
+#define MT48LC4M32B2B5_SDCTRL               (SDRAM_COLUMN_8BIT        |\
+                                                SDRAM_ROW_13BIT          |\
+                                                SDRAM_MEM_BUS_WIDTH_32   |\
+                                                SDRAM_INTERN_BANKS_NUM_4 |\
+                                                SDRAM_CAS_LATENCY_3      |\
+                                                SDRAM_WRITE_PROTECTION_DISABLE |\
+                                                SDRAM_CLOCK_PERIOD_2     |\
+                                                SDRAM_RBURST_ENABLE      |\
+                                                SDRAM_RPIPE_DELAY_0)
+
+#define SDRAM_CTRL_PARAM                    MT48LC4M32B2B5_SDCTRL
+
+#define LOAD_TO_ACTIVE_DELAY                2
+#define EXIT_SELF_REFRESH_DELAY             7
+#define SELF_REFRESH_TIME                   4
+#define ROW_CYCLE_DELAY                     7
+#define WRITE_RECOVERY_TIME                 2
+#define RPDELAY                             2
+#define RCD_DELAY                           2
 #endif
+
+#endif
+
+
